32df0c07317ed6c8979d9f552107dc976b8bae18
Subodh Tiwari <subodhtiwari360@gmail.com>
2024-07-02 23:05:25 +1000

    Close bottom sheet when clicking outside or navigating to next page

--- components/screens/rooms/listView/listView.jsx
+++ components/screens/rooms/listView/listView.jsx
@@ -1,8 +1,15 @@
-import React, { useRef, useState } from "react";
-import { View, Text, FlatList, Dimensions } from "react-native";
+import React, { useRef, useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { styles } from "./listView.style";
 import BottomSheet from "react-native-simple-bottom-sheet";
 import ItemCard from "../../../cards/itemcard";
+import { useFocusEffect } from "@react-navigation/native";
 
 const ListView = ({ markers }) => {
   console.log("list view is rendered");
@@ -10,43 +17,56 @@ const ListView = ({ markers }) => {
   const [selectedItem, setSelectedItem] = useState(null);
   const { height: screenHeight } = Dimensions.get("window");
   const bottomSheetHeight = screenHeight * 0.65;
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        bottomSheetRef?.current && bottomSheetRef?.current?.togglePanel();
+      };
+    }, [])
+  );
   const handlePress = (item) => {
     setSelectedItem(item);
-    setTimeout(() => {
-      bottomSheetRef.current.togglePanel();
-    }, 100);
+    bottomSheetRef?.current?.togglePanel();
+  };
+  const handleOutsidePress = () => {
+    bottomSheetRef?.current && bottomSheetRef?.current?.togglePanel();
   };
   return (
-    <View>
-      <FlatList
-        data={markers}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <ItemCard
-            title={item.title}
-            description={item.description}
-            phone={item.phone}
-            price={item.price}
-            date={item.date}
-            onPress={() => handlePress(item)}
-          />
-        )}
-      />
-      <BottomSheet
-        ref={bottomSheetRef}
-        isOpen={false}
-        sliderMinHeight={0}
-        sliderMaxHeight={bottomSheetHeight}
-      >
-        <View style={styles.bottomSheetContainer}>
-          {selectedItem && (
-            <Text style={[styles.sheetText, { height: bottomSheetHeight }]}>
-              {selectedItem.title}
-            </Text>
+    <TouchableWithoutFeedback onPress={handleOutsidePress}>
+      <View>
+        <FlatList
+          data={markers}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <ItemCard
+              title={item.title}
+              description={item.description}
+              phone={item.phone}
+              price={item.price}
+              date={item.date}
+              onPress={() => handlePress(item)}
+            />
           )}
-        </View>
-      </BottomSheet>
-    </View>
+        />
+        <BottomSheet
+          ref={bottomSheetRef}
+          isOpen={false}
+          sliderMinHeight={0}
+          sliderMaxHeight={bottomSheetHeight}
+          animationDuration={200}
+        >
+          <TouchableWithoutFeedback>
+            <View style={styles.bottomSheetContainer}>
+              {selectedItem && (
+                <Text style={[styles.sheetText, { height: bottomSheetHeight }]}>
+                  {selectedItem.title}
+                </Text>
+              )}
+            </View>
+          </TouchableWithoutFeedback>
+        </BottomSheet>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
